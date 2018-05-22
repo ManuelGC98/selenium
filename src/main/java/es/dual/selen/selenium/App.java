4940b48e41222d51d5e916d0ff7d30a9be8a2d9d
formacion <formacion@192.168.61.133>
2018-05-22 10:17:46 +0200

    22/5 10:20

--- src/main/java/es/dual/selen/selenium/App.java
+++ src/main/java/es/dual/selen/selenium/App.java
@@ -1,9 +1,22 @@
 package es.dual.selen.selenium;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        DriverSeleniumFirefox firefox = new DriverSeleniumFirefox();
-    }
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+
+		DriverSeleniumFirefox driver = new DriverSeleniumFirefox();
+		driver.getDriver().get("https://www.google.es");
+
+		driver.getDriver().findElement(By.id("lst-ib")).sendKeys("Lista convocados seleccion española mundial 2018");
+		driver.getDriver().findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+		System.out.println(driver.getDriver().findElement(By.id("resultStats")).getText());
+		driver.getDriver().findElement(By.className("RTVE")).click();
+		
+		driver.cerrarProceso();
+	}
 }

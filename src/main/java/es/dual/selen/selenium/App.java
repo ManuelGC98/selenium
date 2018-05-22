70c79b4fac15b1ff28f29f71c0064f418119fb35
formacion <formacion@192.168.61.133>
2018-05-22 13:06:50 +0200

    22/5 13:10

--- src/main/java/es/dual/selen/selenium/App.java
+++ src/main/java/es/dual/selen/selenium/App.java
@@ -28,8 +28,17 @@ public class App {
 		// driver.getDriver().findElement(By.linkText("PrimeFaces Ultima")).click();
 		
 		driver.getDriver().get("https://www.primefaces.org/ultima/");
-		driver.getDriver().findElement(By.linkText("PrimeFaces Ultima")).click();
 		driver.getDriver().findElement(By.id("menuform:um_components")).click();
+		driver.getDriver().findElement(By.id("menuform:um_messages")).click();
+		driver.getDriver().findElement(By.id("j_idt51:j_idt55")).click();
+		driver.getDriver().findElement(By.id("j_idt51:default")).sendKeys("A");
+		driver.getDriver().findElement(By.id("j_idt51:txt")).sendKeys("A");
+		driver.getDriver().findElement(By.id("j_idt51:icon")).sendKeys("A");
+		driver.getDriver().findElement(By.id("j_idt51:inline")).sendKeys("A");
+		driver.getDriver().findElement(By.id("j_idt51:mtext")).click();
+		driver.getDriver().findElement(By.id("j_idt51:mtext_1")).click();
+		driver.getDriver().findElement(By.id("j_idt51:micon")).sendKeys("A");
+		driver.getDriver().findElement(By.id("j_idt51:submitButton")).click();
 		
 		driver.cerrarProceso();
 	}
